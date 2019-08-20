1946062d8af8515876d1977e3bd9b2a6a9543d34
schwafunk <54252610+schwafunk@users.noreply.github.com>
2019-08-19 21:08:37 -0400

    Added Schwa_Launchpad_Test based on St4rchild template

---
+++ Schwa_Launchpad_Test/__init__.py
@@ -0,0 +1,13 @@
+# emacs-mode: -*- python-*-
+# -*- coding: utf-8 -*-
+
+import Live
+from Schwa_Launchpad_Test import Schwa_Launchpad_Test
+
+def create_instance(c_instance):
+    ' Creates and returns the APC20 script '
+    return Schwa_Launchpad_Test(c_instance)
+
+
+# local variables:
+# tab-width: 4
